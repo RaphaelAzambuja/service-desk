fa56b219c669afe4f97325e3cfeee4178abfc5c1
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-04-25 14:48:41 -0300

    fix: Criado DefaultRepository

---
+++ app/repositories/default_repository.rb
@@ -0,0 +1,17 @@
+class DefaultRepository
+  def find_all
+    User.where(deleted_at: nil)
+  end
+
+  def find_by_id(id)
+    User.find(id)
+  end
+
+  def create(params)
+    User.create(params)
+  end
+
+  def update(params)
+    User.update(params)
+  end
+end
--- app/repositories/user_repository.rb
+++ app/repositories/user_repository.rb
@@ -1,17 +1,2 @@
-class UserRepository
-  def find_all
-    User.where(deleted_at: nil)
-  end
-
-  def find_by_id(id)
-    User.find(id)
-  end
-
-  def create(params)
-    User.create(params)
-  end
-
-  def update(params)
-    User.update(params)
-  end
+class UserRepository < DefaultRepository
 end
